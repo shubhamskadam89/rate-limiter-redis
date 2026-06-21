ffefb8fd9ca0532088adc974fd84fb4da73cb6be
Shubham Kadam <shubhamskadam89@gmail.com>
2026-06-21 12:09:02 +0530

    feat: add backend maven skeleton and setup gitignore

---
+++ backend/src/main/java/com/shubham/flashsale/FlashSaleEngineApplication.java
@@ -0,0 +1,14 @@
+package com.shubham.flashsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashSaleEngineApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(FlashSaleEngineApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/shubham/flashsale/FlashSaleEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.flashsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashSaleEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
